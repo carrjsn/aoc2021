6061ca4e5a9555476e24da55f843267d180f2ff6
Jason Carr <carrjsn@gmail.com>
2021-12-02 21:17:11 -0800

    complete day1 pt2 aoc

---
+++ day1-pt2.py
@@ -0,0 +1,22 @@
+
+def main():
+
+  with open('day1.txt', 'r') as file:
+    lines = file.readlines()
+
+  nums = list(map(lambda l : int(l.replace('\n', '')), lines))
+
+  increasing = 0
+  prev_sum = sum(nums[:3])
+
+  for i in range(1, len(nums)):
+    currSum = sum(nums[i:i+3])
+    if currSum > prev_sum:
+      increasing += 1
+    prev_sum = currSum
+
+  print(increasing)
+
+
+if __name__ == '__main__':
+  main()
