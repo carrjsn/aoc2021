715dfba5d0ff02c50342d586342f34cc61fbd1cc
Jason Carr <carrjsn@gmail.com>
2021-12-02 21:44:22 -0800

    complete day2 pt 1

---
+++ day2-pt1.py
@@ -0,0 +1,31 @@
+def main():
+
+  with open('day2.txt', 'r') as file:
+    lines = file.readlines()
+
+  moves = list(map(lambda l : l.replace('\n', ''), lines))
+
+  dirs = {
+    'forward': 0,
+    'up': 0,
+    'down': 0
+  }
+
+  for move in moves:
+    tup = move.split(' ')
+    if tup[0] == 'up':
+      dirs['up'] -= int(tup[1])
+    else:
+      dirs[tup[0]] += int(tup[1])
+
+  depth = dirs['down'] + dirs['up']
+  final_position = dirs['forward'] * depth
+
+
+  print('dirs', dirs)
+  print('depth', depth)
+  print('final', final_position)
+
+
+if __name__ == '__main__':
+  main()
