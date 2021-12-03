627b2b9babce3cf001a220d97200faff1097badb
Jason Carr <carrjsn@gmail.com>
2021-12-02 20:59:56 -0800

    complete day1 aoc 2021

---
+++ main.py
@@ -0,0 +1,41 @@
+# import pandas as pd
+
+
+def main():
+
+  with open('day1.txt', 'r') as file:
+    lines = file.readlines()
+
+  increasing = 0
+
+  for i in range(1, len(lines)):
+    if (int(lines[i].replace('\n', '')) > int(lines[i-1].replace('\n', ''))):
+      increasing += 1
+
+  print(increasing)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  main()
+
