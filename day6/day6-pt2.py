79421d78a7b0a846d44ec0611e323d8f5ac3750f
Jason Carr <carrjsn@gmail.com>
2021-12-09 21:48:22 -0800

    every 7 days lantern refactor

--- day6/day6-pt2.py
+++ day6/day6-pt2.py
@@ -1,22 +1,121 @@
 def main():
 
-  with open('day6/day6.txt', 'r') as file:
+  with open('day6/day6example.txt', 'r') as file:
     lines = file.readlines()
 
-  fish = list(map(int, lines[0].split(',')))
-  print(fish)
+  # list of strings
+  fish = list(lines[0].split(','))
+  # print(fish)
 
+  # for incrementing by 7
+  num_map = {
+    '0': '2',
+    '1': '3',
+    '2': '4',
+    '3': '5',
+    '4': '6',
+    '5': '7',
+    '6': '8',
+    '7': '0',
+    '8': '1'
+  }
+
+
+  # string_fish =
   # ints for tracking how many new fish a 'chunk' makes every 7 days
   # regens = []
+  # string fish '34312'
+  for _ in range(2):
 
-  for i in range(256):
-    zero_count = fish.count(0)
-    fish = [n - 1 if n > 0 else 6 for n in fish]
-    for i in range(zero_count):
-      fish.append(8)
+    # store sorted fish to represent the potentially new spawned fish (aka --  all fish with timers at 6 or less)
+    sorted_fish = sorted(fish.copy())
 
-  print('after 80', len(fish))
+    # maybe dont need zero count?
+    # zero_count = fish.count('0')
+
+    # THIS is for generating new chunk to be concatted
+    new_fish = []
+
+    for i in range(len(sorted_fish)):
+      # ignore 7s and 8s because they won't spawn in 7 days
+      if int(sorted_fish[i]) < 7:
+        new_fish.append(num_map[sorted_fish[i]])
+      else:
+        # replace 7s with 0s and 8s with 1s on ORIGNAL fish list (first chunk) - leave all other numbers the same??
+        # not sure if this works here...
+        fish[i] = num_map[fish[i]]
+
+    # replace 7s with 0s and 8s with 1s on ORIGNAL fish list (first chunk) - leave all other numbers the same
+
+    # fish = concat old fish + sorted_fish
+    fish = fish + new_fish
+
+    print('fish', ''.join(fish))
+
+
+  # NOW figure out how far away from the target days you are
+  # test - example 4 away from 18
+
+  # iterate 4 times
+
+
+
+
+
+  print('after', len(fish))
 
 
 if __name__ == '__main__':
   main()
+
+
+
+  # string_fish = ''.join(list(lines[0].split(',')))
+  # # print(fish)
+
+  # # for incrementing by 7
+  # # num_map = {
+  # #   0: '2',
+  # #   1: '3',
+  # #   2: '4',
+  # #   3: '5',
+  # #   4: '6',
+  # #   5: '7',
+  # #   6: '8',
+  # #   7: '0',
+  # #   8: '1'
+  # # }
+
+  # # for incrementing one at a time
+  # num_map = {
+  #   '0': '6',
+  #   '1': '0',
+  #   '2': '1',
+  #   '3': '2',
+  #   '4': '3',
+  #   '5': '4',
+  #   '6': '5',
+  #   '7': '6',
+  #   '8': '7',
+  # }
+
+  # # string_fish =
+  # # ints for tracking how many new fish a 'chunk' makes every 7 days
+  # # regens = []
+  # # string fish '34312'
+  # for _ in range(256):
+  #   # store curr fish arr
+
+  #   zero_count = string_fish.count('0')
+
+  #   # set to 7 so that it's actually correctly set to 6 after iteration below
+  #   string_fish = string_fish.replace('0', '7')
+
+  #   for key, val in num_map.items():
+  #     string_fish = string_fish.replace(key, val)
+
+  #   string_fish += '8' * zero_count
+
+  #   # sort fish nums
+  #   # fish_sort = ''.join(sorted(list(curr_fish)))
+  #   # make new arr for adding on to original
