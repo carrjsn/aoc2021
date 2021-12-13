ba93542b1fdd7b51d86e20a6760488e213e77290
Jason Carr <carrjsn@gmail.com>
2021-12-12 21:09:44 -0800

    solve day7-pt1

--- day7/day7-pt1.py
+++ day7/day7-pt1.py
@@ -6,6 +6,19 @@ def main():
 
   print(nums)
 
+  least_fuel = float('inf')
+
+  for i in range(len(nums)):
+    curr_crab = nums[i]
+    curr_fuel = 0
+    # compare distance from all other crabs
+    for j in range(len(nums)):
+      temp_crab = nums[j]
+      curr_fuel += abs(curr_crab - temp_crab)
+    if curr_fuel < least_fuel:
+      least_fuel = curr_fuel
+
+  print(least_fuel)
 
 if __name__ == '__main__':
   main()
