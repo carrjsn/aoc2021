387f8d16381ac912e189ad9a931576cfd3a449d5
Jason Carr <carrjsn@gmail.com>
2021-12-17 20:27:39 -0800

    solve day10-pt2

--- day10/day10-pt2.py
+++ day10/day10-pt2.py
@@ -1,28 +1,35 @@
+import math
+
 def main():
 
   with open('day10/day10.txt') as file:
     files = list(map(lambda line : line.replace('\n', ''), file.readlines()))
 
     char_vals = {
-      ')': 3,
-      ']': 57,
-      '}': 1197,
-      '>': 25137
+      ')': 1,
+      ']': 2,
+      '}': 3,
+      '>': 4
     }
 
     char_matches = {
       ')': '(',
       ']': '[',
       '}': '{',
-      '>': '<'
+      '>': '<',
+      '(': ')',
+      '[': ']',
+      '{': '}',
+      '<': '>'
     }
 
-    illegals = []
 
-    recent_openings = []
+    completion_strings = []
     # itereate over input one row/line at a time
     # detect corrupted lines - break if found and add the illegal char to illegals
     for line in files:
+      recent_openings = []
+      line_corrupted = False
       for char in line:
         # check if curr char is an opening
         if char in '({[<':
@@ -32,16 +39,34 @@ def main():
         elif char in ')}]>':
           # make sure it matches the last index of recent_openings
           if not len(recent_openings) or recent_openings[-1] != char_matches[char]:
-            # if it does not - the line is corrupted - add the illegal char to illegals
-            illegals.append(char)
+            # if it does not - the line is corrupted - break out of loop and move onto next line
+            line_corrupted = True
             break
           # if vaild match remove most recent so that next in line is expected
           else:
             recent_openings.pop()
 
+      # if no illegals found - it is a incomplete
+      if not line_corrupted:
+        # create the completion string that makes a valid line
+        curr_completion_str = ''
+        for ch in recent_openings[::-1]:
+          curr_completion_str += char_matches[ch]
+        # add to global list
+        completion_strings.append(curr_completion_str)
 
-    print(sum(map(lambda ch : char_vals[ch], illegals)))
 
+    print(completion_strings)
+
+    scores = []
+    for item in completion_strings:
+      score = 0
+      for ch in item:
+        score = score * 5 + char_vals[ch]
+      scores.append(score)
+
+    mid_idx = math.floor(len(scores) / 2)
+    print(sorted(scores)[mid_idx])
 
 
 if __name__ == '__main__':
