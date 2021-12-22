df0c2f401def8f4fa29f5481844da584fe9d4c42
Jason Carr <carrjsn@gmail.com>
2021-12-21 20:07:01 -0800

    set up dict for storing edges

--- day12/day12-pt1.py
+++ day12/day12-pt1.py
@@ -1,7 +1,28 @@
 def main():
-  with open('day12/day12.txt') as file:
-    files = list(map(lambda line : line.replace('\n', ''), file.readlines()))
-  print(files)
+  with open('day12/day12example.txt') as file:
+    connections = list(map(lambda line : line.replace('\n', ''), file.readlines()))
+  # print(connections)
+
+  # like a graph -- track edges of each node
+  # make dict
+  edges = { }
+
+  for connection in connections:
+    first, second = connection.split('-')
+    if first not in edges:
+      edges[first] = [second]
+    else:
+      edges[first].append(second)
+    if second not in edges:
+      edges[second] = [first]
+    else:
+      edges[second].append(first)
+
+  for key in edges:
+    print(key, edges[key])
+
+
+
 
 
 
