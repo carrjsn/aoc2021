d550defced718fc7da74712e9a6540ef7a12cfb3
Jason Carr <carrjsn@gmail.com>
2022-01-05 21:01:39 -0800

    solve day12-pt1

--- day12/day12-pt1.py
+++ day12/day12-pt1.py
@@ -1,5 +1,5 @@
 def main():
-  with open('day12/day12example.txt') as file:
+  with open('day12/day12.txt') as file:
     connections = list(map(lambda line : line.replace('\n', ''), file.readlines()))
   # print(connections)
 
@@ -23,16 +23,12 @@ def main():
 
   # helper function - define up here inside main for closure access to edges dictionary
   def find_valid_paths(path, ele):
-    print(path)
     path_count = 0
 
     # base case
     if ele == 'end':
       path_count += 1
-      print('success', path)
       return path_count
-    # elif len(edges[ele]) == 1 and edges[ele][0] in path and edges[ele][0].islower():
-    #   return 0
 
     # itereate over arr of edges
     for edge in edges[ele]:
@@ -41,9 +37,9 @@ def main():
       if (edge.islower() and edge in path) or edge == 'start':
         continue
       else:
-        # add the current edge to the path
-        path.append(edge)
-        path_count += find_valid_paths(path, edge)
+        # don't change input path, but make new unique path for recursion - with the current edge appended to it
+        new_path = path + [edge]
+        path_count += find_valid_paths(new_path, edge)
 
     return path_count
 
@@ -54,13 +50,10 @@ def main():
   # iterate over edges[start] and look for valid paths to end
   for edge in edges['start']:
     # find number of valid paths to end for each element
-
     path = ['start', edge]
     valid_paths += find_valid_paths(path, edge)
 
   print('valid paths', valid_paths)
-
-
 
 
 
